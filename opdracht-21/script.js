6e6f1001330d11632c211f670660738a40fb398e
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-11-21 19:19:20 +0100

    Add files via upload

---
+++ opdracht-21/script.js
@@ -0,0 +1,46 @@
+console.log("Hallo Winc Academy studenten");
+/*let sum = 1230941 * 1823794;
+console.log(sum);
+let division = 637263 / 54;
+console.log(division);*/
+// dit is een regel comment met shortcut cmd + k + c
+
+// let myAge = 19;
+
+// if (myAge > 30) {
+
+//     console.log("you are over 30!");
+
+// } else if (myAge > 20) {
+//     console.log("you are over 20!");
+
+// } else if (myAge > 10) {
+//     console.log("you are over 10!");
+
+// } else {
+//     console.log("you are not over 10!")
+// }
+
+let yourAge = 29;
+
+if (yourAge >= 18) {
+    console.log("Je bent 18 jaar of ouder en mag naar binnen");
+} else {
+    console.log("Je bent nog geen 18 jaar en mag niet naar binnen");
+}
+
+let isFemale = false;
+
+if (isFemale) {
+    console.log("Welkom op de ladiesnight");
+} else {
+    console.log("Je zult op een andere avond terug moeten komen");
+}
+
+let driverStatus = "bob";
+
+if (driverStatus = "bob") {
+    console.log("Je bent de bob en mag rijden")
+} else {
+    console.log("Je bent niet de bob en mag niet rijden")
+}
