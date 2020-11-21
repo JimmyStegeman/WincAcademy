6e98b7c3c1172b3b3801bbba07f592bdc6bb8016
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-11-21 14:22:58 +0100

    Add files via upload

---
+++ opdracht-20/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten")
+/*let sum = 1230941 * 1823794
+console.log(sum)
+let division = 637263 / 54
+console.log(division)*/
+// dit is een regel comment met shortcut cmd + k + c
