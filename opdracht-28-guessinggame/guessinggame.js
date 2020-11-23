03fd7113b5199bc72bf947b3413c06934d37da32
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-11-23 15:39:48 +0100

    Add files via upload

---
+++ opdracht-28-guessinggame/guessinggame.js
@@ -0,0 +1,23 @@
+//input name
+let person = prompt("Welkom! Wat is je naam?")
+
+//if anything is filled in then reply with greeting
+if (person != null) {
+    console.log("Hi " + person + "!");
+}
+
+//if anything is filled in then reply with greeting
+let guessingGame = prompt("Voer een nummer in van 0 tot 25 om te beginnen met raden...");
+
+// random number 
+let awnser = Math.floor(Math.random() * 25 + 1);
+
+if (guessingGame == awnser) {
+    console.log("GEFELICITEERD! Je hebt het getal geraden!");
+    console.log("Dag " + person + ". Tot de volgende keer!");
+} else if (guessingGame > awnser) {
+    console.log("Oops! Probeer eens een kleiner getal.");
+} else {
+    console.log("Oops! Probeer eens een groter getal. ");
+}
+
