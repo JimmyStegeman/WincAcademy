37e5ab98534ecc5bb9d98afc0be6359c9c6daffc
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-11-23 14:49:14 +0100

    Add files via upload

--- opdracht-27-loops-2/script.js
+++ opdracht-27-loops-2/script.js
@@ -1,3 +1,22 @@
-let colors = ["yellow", "blue", "red", "orange"];
+const colors = ["yellow", "blue", "red", "orange"];
 
-colors.forEach(element => console.log(element))
+//while loop
+
+let i = 0
+while (i < colors.length) {
+    console.log(colors[i]);
+    i++;
+}
+
+//for loop
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors);
+}
+
+//array forEach
+colors.forEach((color) => console.log(color));
+
+// 1. mijn while loop neemt 5 regels in beslag en mijn for loop 3 regels
+// 2. 1 regel
+// 3. leesbaarder door een regel ook eenvoudiger geen gebruik van extra parameter i, maar direct de inhoud van de array aanspreken.
