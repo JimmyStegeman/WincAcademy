daf8f2f65b5212f181c719d46f5675dfde22b741
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-12-27 16:14:56 +0100

    Add files via upload

---
+++ opdracht-33/script.js
@@ -0,0 +1,20 @@
+// // zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = function () {
+//     console.log("Joe, ik rock de arrow functions!");
+//   };
+
+//   ikRockArrowFunctions()
+
+const ikRockArrowFunctions = () => ("Joe, ik rock de arrow functions!")
+
+// const fivePlusSeven = function () {
+//     return 5 + 7
+//   };
+
+//   fivePlusSeven();
+
+const fivePlusSeven = () => 5 + 7
+
+const myFunction = (a, b) => 6 + 8
+
+const addFive = a => a + 5
