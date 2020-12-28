8a1d4948e04a8c3ff734e2d087622a3f0743d2db
Jimmy Stegeman <74182954+JimmyStegeman@users.noreply.github.com>
2020-12-28 11:42:18 +0100

    Add files via upload

---
+++ opdracht-35/script.js
@@ -0,0 +1,40 @@
+//a
+const addTheWordCool = ["nice", "awesome", "geweldig"];
+
+//addTheWordCool.push("cool");
+
+console.log(addTheWordCool.push("cool"))
+
+
+//b
+const amountOfElementsInArray = ["appels", "peren", "citroenen"];
+
+console.log(amountOfElementsInArray.length);
+
+//c
+const selectBelgiumFromBenelux = ["Belgie", "Nederland", "Luxemburg"];
+
+console.log(selectBelgiumFromBenelux[0]);
+
+//d
+const lastElementInArray = ["Haas", "Cavia", "Kip", "Schildpad"];
+
+console.log(lastElementInArray[lastElementInArray.length - 1]);
+
+//e
+const presidents = ["Trump", "Obama", "Bush", "Clinton"];
+
+const impeachTrumpSlice = function (presidents) {
+    [0, 1]
+}
+
+const impeachTrumpSplice = function (presidents) {
+    [0, 1]
+}
+
+console.log(impeachTrumpSlice)
+console.log(impeachTrumpSplice)
+
+//f
+
+//g
